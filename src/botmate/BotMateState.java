1dd6496b439c315a9f62634b77af6cefcc36b4e2
Andy LE <anhlv2312@gmail.com>
2018-08-27 11:56:36 +1000

    Create new skeleton function

--- src/botmate/BotMateState.java
+++ src/botmate/BotMateState.java
@@ -1,8 +1,13 @@
 package botmate;
 
 import common.State;
+import common.StateCostPair;
+
 import problem.Box;
 import problem.RobotConfig;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BotMateState {
@@ -13,6 +18,7 @@ public class BotMateState {
     /** A list of moving boxes and obstacles */
     private List<Box> movingBoxes;
     private List<Box> movingObstacles;
+    private Box currentBox;
 
     /**
      * Create an problem state from an RobotConfig and list of moving objects
@@ -39,9 +45,52 @@ public class BotMateState {
 
     public boolean isConnectedWith(BotMateState nextState) {
         //todo: check if the robot and moving object can move from this state to the next state
+
+        // draw boundary of the current moving objects (this.active and nextState.active)
+        // draw a rectangle that cover both of them,
+        // check collision with any other obstacle
+
         return false;
     }
 
+    /**
+     * Return a list of all states reachable from this state.
+     * @return list of successors
+     */
+    public List<StateCostPair> getSuccessors(List<State> samples, State goal) {
+        List<StateCostPair> successors = new ArrayList<StateCostPair>();
 
+        // First get some of the samples (which are close to the current state)
+
+
+        // Try to connect them
+        // if is connecteed, calculate cost and heuristic to the goal using manhatan and add to succssors
+        // then return successor
+
+        return successors;
+    }
+
+    /**
+     * Check if this puzzle state is equivalent to state s
+     * @param s state to check equality with
+     * @return true if all tiles are in the same position
+     */
+    private boolean isDone(BotMateState s) {
+
+        return this.heuristic(s) <= 0.0000001;
+
+    }
+
+
+    /**
+     * An estimation of cost from current state to s. The number of different cells divide by 2 which is admissible.
+     * @param s
+     * @return a double number
+     */
+    public double heuristic(BotMateState s) {
+        calculateDistance(this.currentBox, s.currentBox);
+        //todo: use manhatan distance to calculate the distance currentBox of its goal
+        return 1;
+    }
 
 }
---
+++ src/botmate/BotMateUtility.java
@@ -0,0 +1,23 @@
+package botmate;
+import problem.Box;
+
+import java.awt.geom.Point2D;
+
+public class BotMateUtility {
+
+    public double calculateDistance(Point2D intial, Point2D goal) {
+        return 0;
+    }
+
+    public Box createBoundaryBox(Box box, double margin) {
+        return null;
+    }
+
+    public Box createCoverBox(Box box1, Box Box2) {
+        return null;
+    }
+
+    public boolean checkCollisionBetweenBox(Box box1, Box Box2) {
+        return false;
+    }
+}
